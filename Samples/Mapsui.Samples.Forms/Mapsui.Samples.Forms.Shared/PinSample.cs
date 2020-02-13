885bf8342fc8ef5c714bfbd8af6fb4b62278e56d
Dirk Weltz <web@weltz-online.de>
2020-02-13 17:00:15 +0100

    Added Padding to Forms Pin sample

--- Samples/Mapsui.Samples.Forms/Mapsui.Samples.Forms.Shared/PinSample.cs
+++ Samples/Mapsui.Samples.Forms/Mapsui.Samples.Forms.Shared/PinSample.cs
@@ -64,6 +64,7 @@ namespace Mapsui.Samples.Forms
                     pin.Callout.ArrowWidth = rnd.Next(0, 20);
                     pin.Callout.ArrowAlignment = (ArrowAlignment)rnd.Next(0, 4);
                     pin.Callout.ArrowPosition = rnd.Next(0, 100) / 100;
+                    pin.Callout.Padding = new Thickness(rnd.Next(0, 20), rnd.Next(0,20));
                     pin.Callout.BackgroundColor = Color.White;
                     pin.Callout.Color = pin.Color;
                     if (rnd.Next(0, 3) < 2)
